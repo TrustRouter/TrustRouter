0e679524d22ecc052dab5445d49c727c9bffd1f1
Martin <mkaydev@googlemail.com>
2012-02-16 01:35:15 +0100

    changed api to accept file_path for CAs in PEM-Format, list of DER-encoded certs for untrusted and DER-encoded cert

--- client/usermode/RAverification/__init__.py
+++ client/usermode/RAverification/__init__.py
@@ -5,6 +5,8 @@ import os
 import sys
 import platform
 from ctypes import CDLL, c_char_p, c_int
+import base64
+import tempfile
 
 module_path = os.path.abspath(__file__)
 module_directory = os.path.split(module_path)[0]
@@ -51,6 +53,12 @@ _verify_prefix_with_cert = libsecurity.verify_prefix_with_cert
 _verify_prefix_with_cert.argtypes = [c_char_p, c_char_p, c_char_p, c_char_p]
 _verify_prefix_with_cert.restype = c_int
 
+def _der_to_pem(cert_der):
+    cert_pem = "-----BEGIN CERTIFICATE-----\n".encode("ascii")
+    cert_pem += base64.encodebytes(cert_der)
+    cert_pem += "-----END CERTIFICATE-----\n".encode("ascii")
+    return cert_pem
+
 def _format_to_bytes(string):
     if string == None:
         return string
@@ -78,48 +86,85 @@ def _get_ipaddrblock_ext(prefix, prefix_length):
     ext += str(prefix_length)
     return ext
 
+def _create_temp_untrusted(untrusted_certs):
+    untrusted_file_tuple = tempfile.mkstemp()
+    untrusted_file_handle = untrusted_file_tuple[0]
+    untrusted_path = untrusted_file_tuple[1]
+    untrusted_file = os.fdopen(untrusted_file_handle, "wb")
+    for cert_der in untrusted_certs:
+        untrusted_file.write(_der_to_pem(cert_der))
+    untrusted_file.close()
+    return untrusted_path
+
+def _create_temp_cert(cert_der):
+    cert_file_tuple = tempfile.mkstemp()
+    cert_file_handle = cert_file_tuple[0]
+    cert_path = cert_file_tuple[1]
+    cert_file = os.fdopen(cert_file_handle, "wb")
+    cert_file.write(_der_to_pem(cert_der))
+    cert_file.close()
+    return cert_path
+
 # OpenSSL Return-Value  : bool
 #               0       : False
 #               1       : True
 #           -1 (error)  : False --> better a false negative than a false positive
 
-# str(path_to_file), str(path_to_file) or None, str(path_to_file)  
-def verify_cert(CAcert_path, untrusted_certs_path, cert_path):
-    valid = _verify_cert(
-        _format_to_bytes(CAcert_path),
-        _format_to_bytes(untrusted_certs_path),      
-        _format_to_bytes(cert_path)  
-    )
+# str(path_to_PEM_file), list<bytes(DER-encoded cert)>, bytes(DER-encoded cert)  
+def verify_cert(CAcert_path, untrusted_certs, cert):
+    tmp_untrusted_path = _create_temp_untrusted(untrusted_certs)
+    tmp_cert_path = _create_temp_cert(cert)
+    valid = -1
+    try:
+        valid = _verify_cert(
+            _format_to_bytes(CAcert_path),
+            _format_to_bytes(tmp_untrusted_path),      
+            _format_to_bytes(tmp_cert_path)  
+        )
+    finally:
+        os.remove(tmp_untrusted_path)
+        os.remove(tmp_cert_path)
     return 0 < valid
 
 # CA and untrusted are needed, because the resources in cert could be inherited
-# str(path_to_file), str(path_to_file) or None, str(path_to_file), bytearray(prefix), int(prefix_length)
-def verify_prefix_with_cert(CAcert_path, untrusted_certs_path, cert_path, prefix, prefix_length):
+# str(path_to_PEM_file), list<bytes(DER-encoded cert)>, bytes(DER-encoded cert), bytearray(prefix), int(prefix_length)
+def verify_prefix_with_cert(CAcert_path, untrusted_certs, cert, prefix, prefix_length):
     prefix_ext = _get_ipaddrblock_ext(bytes(prefix), prefix_length)
-    valid = _verify_prefix_with_cert(
-        _format_to_bytes(CAcert_path),
-        _format_to_bytes(untrusted_certs_path),
-        _format_to_bytes(cert_path),
-        _format_to_bytes(prefix_ext)
-    )
+    tmp_untrusted_path = _create_temp_untrusted(untrusted_certs)
+    tmp_cert_path = _create_temp_cert(cert)
+    valid = -1
+    try:
+        valid = _verify_prefix_with_cert(
+            _format_to_bytes(CAcert_path),
+            _format_to_bytes(tmp_untrusted_path),
+            _format_to_bytes(tmp_cert_path),
+            _format_to_bytes(prefix_ext)
+        )
+    finally:
+        os.remove(tmp_untrusted_path)
+        os.remove(tmp_cert_path)
     return 0 < valid
 
-def verify_signature(signing_cert_path, signed_data, signature):
-    # signing_cert_path :   path to the certificate which contains the public key,
+def verify_signature(signing_cert, signed_data, signature):
+    # signing_cert      :   DER-encoded certificate which contains the public key,
     #                       corresponding to the private key, which was used to sign
     # signed_data       :   raw package data (bytes) which were signed to create 
     #                       the signature
     # signature         :   supposedly rsa_pkcs1_1.5 signature over sha1(signed_data)
-    signing_cert_path = _format_to_bytes(signing_cert_path)
+    tmp_cert_path = _create_temp_cert(signing_cert)
+    signing_cert_path = _format_to_bytes(tmp_cert_path)
     signature = bytes(signature)
     signed_data = bytes(signed_data) 
-
-    signed = _verify_signature(
-        signing_cert_path, 
-        signature, 
-        signed_data, 
-        len(signed_data)
-    )
+    signed = -1
+    try:
+        signed = _verify_signature(
+            signing_cert_path, 
+            signature, 
+            signed_data, 
+            len(signed_data)
+        )
+    finally:
+        os.remove(tmp_cert_path)
     return 0 < signed
 
 
--- client/usermode/RAverification/test/__init__.py
+++ client/usermode/RAverification/test/__init__.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # Note: the tests depend on the certificates, which makes them time-dependent, because the certificates will expire at some point
@@ -17,33 +16,80 @@ from RAverification import _format_to_bytes
 o_data_directory = module_directory + "/example_data/" + "only_one_block/"
 m_data_directory = module_directory + "/example_data/" + "multiple_blocks/"
 
-ripe_o = o_data_directory + "ripe/ripe.cer"
-dfn_o = o_data_directory + "dfn/dfn.cer"
-uni_o = o_data_directory + "uni_potsdam/uni_potsdam.cer"
-hpi_o = o_data_directory + "hpi/hpi.cer"
-dfn_uni_hpi_o = o_data_directory + "dfn+uni_potsdam+hpi.cer"
+ripe_o_pem_path = o_data_directory + "ripe/ripe.cer"
+dfn_o_pem_path = o_data_directory + "dfn/dfn.cer"
+uni_o_pem_path = o_data_directory + "uni_potsdam/uni_potsdam.cer"
+hpi_o_pem_path = o_data_directory + "hpi/hpi.cer"
+dfn_uni_hpi_o_path = o_data_directory + "dfn+uni_potsdam+hpi.cer"
+router0_o_pem_path = o_data_directory + "router0/router0.cer"
+router1_o_pem_path = o_data_directory + "router1_correct/router1.cer"
+router2_o_pem_path = o_data_directory + "router2_faulty_range/router2.cer"
+router3_o_pem_path = o_data_directory + "router3_faulty_selfsigned/router3.cer"
 
-router0_o = o_data_directory + "router0/router0.cer"
-router1_o = o_data_directory + "router1_correct/router1.cer"
-router2_o = o_data_directory + "router2_faulty_range/router2.cer"
-router3_o = o_data_directory + "router3_faulty_selfsigned/router3.cer"
+ripe_o_der_path = o_data_directory + "ripe/ripe.der"
+dfn_o_der_path = o_data_directory + "dfn/dfn.der"
+uni_o_der_path = o_data_directory + "uni_potsdam/uni_potsdam.der"
+hpi_o_der_path = o_data_directory + "hpi/hpi.der"
+router0_o_der_path = o_data_directory + "router0/router0.der"
+router1_o_der_path = o_data_directory + "router1_correct/router1.der"
+router2_o_der_path = o_data_directory + "router2_faulty_range/router2.der"
+router3_o_der_path = o_data_directory + "router3_faulty_selfsigned/router3.der"
+
+ripe_m_pem_path = m_data_directory + "ripe/ripe.cer"
+dfn_m_pem_path = m_data_directory + "dfn/dfn.cer"
+uni_m_pem_path = m_data_directory + "uni_potsdam/uni_potsdam.cer"
+hpi_m_pem_path = m_data_directory + "hpi/hpi.cer"
+dfn_uni_hpi_m_path = m_data_directory + "dfn+uni_potsdam+hpi.cer"
+router0_m_pem_path = m_data_directory + "router0/router0.cer"
+
+ripe_m_der_path = m_data_directory + "ripe/ripe.der"
+dfn_m_der_path = m_data_directory + "dfn/dfn.der"
+uni_m_der_path = m_data_directory + "uni_potsdam/uni_potsdam.der"
+hpi_m_der_path = m_data_directory + "hpi/hpi.der"
+router0_m_der_path = m_data_directory + "router0/router0.der"
 
 signed_ra_path = o_data_directory + "router0/signed_data"
 ra_signature_path = o_data_directory + "router0/signature"
 
-ripe_m = m_data_directory + "ripe/ripe.cer"
-dfn_m = m_data_directory + "dfn/dfn.cer"
-uni_m = m_data_directory + "uni_potsdam/uni_potsdam.cer"
-hpi_m = m_data_directory + "hpi/hpi.cer"
-dfn_uni_hpi_m = m_data_directory + "dfn+uni_potsdam+hpi.cer"
-router0_m = m_data_directory + "router0/router0.cer"
-
 fh = open(signed_ra_path, "rb")
 signed_ra = fh.read()
 fh.close()
-
 fh = open(ra_signature_path, "rb")
 ra_signature = fh.read()
+fh.close()
+
+fh = open(dfn_o_der_path, "rb")
+dfn_o_der = fh.read()
+fh.close()
+fh = open(uni_o_der_path, "rb")
+uni_o_der = fh.read()
+fh.close()
+fh = open(hpi_o_der_path, "rb")
+hpi_o_der = fh.read()
+fh.close()
+fh = open(router0_o_der_path, "rb")
+router0_o_der = fh.read()
+fh.close()
+fh = open(router1_o_der_path, "rb")
+router1_o_der = fh.read()
+fh.close()
+fh = open(router2_o_der_path, "rb")
+router2_o_der = fh.read()
+fh.close()
+fh = open(router3_o_der_path, "rb")
+router3_o_der = fh.read()
+fh.close()
+fh = open(dfn_m_der_path, "rb")
+dfn_m_der = fh.read()
+fh.close()
+fh = open(uni_m_der_path, "rb")
+uni_m_der = fh.read()
+fh.close()
+fh = open(hpi_m_der_path, "rb")
+hpi_m_der = fh.read()
+fh.close()
+fh = open(router0_m_der_path, "rb")
+router0_m_der = fh.read()
 fh.close()
 
 prefix_b = bytearray(b'\x20\x01\x06\x38\x08\x07\x02\x1d\x00\x00\x00\x00\x00\x00\x00\x00')
@@ -54,54 +100,143 @@ prefix_ext_1 = "IPv6:2001:0638::/32"
 
 def test_verify_prefix():
     assert _verify_prefix_with_cert(
-                _format_to_bytes(ripe_o), 
+                _format_to_bytes(ripe_o_pem_path), 
                 None, 
-                _format_to_bytes(dfn_o), 
+                _format_to_bytes(dfn_o_pem_path), 
                 _format_to_bytes(prefix_ext_0)
             ) == 1
     assert _verify_prefix_with_cert(
-                _format_to_bytes(ripe_o),
-                _format_to_bytes(dfn_uni_hpi_o), 
-                _format_to_bytes(router1_o),
+                _format_to_bytes(ripe_o_pem_path),
+                _format_to_bytes(dfn_uni_hpi_o_path), 
+                _format_to_bytes(router1_o_pem_path),
                 _format_to_bytes(prefix_ext_0)
             ) == 1
     assert _verify_prefix_with_cert(
-                _format_to_bytes(ripe_o), 
-                _format_to_bytes(dfn_uni_hpi_o),
-                _format_to_bytes(router1_o),
+                _format_to_bytes(ripe_o_pem_path), 
+                _format_to_bytes(dfn_uni_hpi_o_path),
+                _format_to_bytes(router1_o_pem_path),
                 _format_to_bytes(prefix_ext_1)
             ) == 0
-    assert verify_prefix_with_cert(ripe_o, None, dfn_o, prefix_b, prefix_length) == True
-    assert verify_prefix_with_cert(ripe_o, dfn_o, uni_o, prefix_b, prefix_length) == True
-    assert verify_prefix_with_cert(ripe_o, None, uni_o, prefix_b, prefix_length) == False
-    assert verify_prefix_with_cert(ripe_o, dfn_uni_hpi_o, router1_o, prefix_b, prefix_length) == True
-    assert verify_prefix_with_cert(ripe_o, dfn_uni_hpi_o, router2_o, prefix_b, prefix_length) == False
-    assert verify_prefix_with_cert(ripe_o, dfn_uni_hpi_o, router3_o, prefix_b, prefix_length) == False
-    assert verify_prefix_with_cert(ripe_o, dfn_uni_hpi_o, router1_o, prefix_b, prefix_length - 1) == False
-    assert verify_prefix_with_cert(ripe_o, None, dfn_o, prefix_bad, prefix_length) == False
-    assert verify_prefix_with_cert(ripe_o, dfn_uni_hpi_o, router1_o, prefix_bad, prefix_length) == False
-    assert verify_prefix_with_cert(ripe_m, None, dfn_m, prefix_b, prefix_length) == True
-    assert verify_prefix_with_cert(ripe_m, dfn_m, uni_m, prefix_b, prefix_length) == True
-    assert verify_prefix_with_cert(ripe_m, None, uni_m, prefix_b, prefix_length) == False
-    assert verify_prefix_with_cert(ripe_m, dfn_uni_hpi_m, router0_m, prefix_b, prefix_length) == True
-    assert verify_prefix_with_cert(ripe_m, dfn_uni_hpi_m, router0_m, prefix_b, prefix_length - 1) == False
-    assert verify_prefix_with_cert(ripe_m, None, dfn_m, prefix_bad, prefix_length) == False
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [],
+        dfn_o_der,
+        prefix_b,
+        prefix_length
+    ) == True
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [dfn_o_der],
+        uni_o_der,
+        prefix_b,
+        prefix_length
+    ) == True
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [],
+        uni_o_der,
+        prefix_b,
+        prefix_length
+    ) == False
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [dfn_o_der, uni_o_der, hpi_o_der],
+        router1_o_der,
+        prefix_b,
+        prefix_length
+    ) == True
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [dfn_o_der, uni_o_der, hpi_o_der],
+        router2_o_der,
+        prefix_b,
+        prefix_length
+    ) == False
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [dfn_o_der, uni_o_der, hpi_o_der],
+        router3_o_der,
+        prefix_b,
+        prefix_length
+    ) == False
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [dfn_o_der, uni_o_der, hpi_o_der],
+        router1_o_der,
+        prefix_b,
+        prefix_length - 1
+    ) == False
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [],
+        dfn_o_der,
+        prefix_bad,
+        prefix_length
+    ) == False
+    assert verify_prefix_with_cert(
+        ripe_o_pem_path,
+        [dfn_o_der, uni_o_der, hpi_o_der],
+        router1_o_der,
+        prefix_bad,
+        prefix_length
+    ) == False
+    assert verify_prefix_with_cert(
+        ripe_m_pem_path,
+        [],
+        dfn_m_der,
+        prefix_b,
+        prefix_length
+    ) == True
+    assert verify_prefix_with_cert(
+        ripe_m_pem_path,
+        [dfn_m_der],
+        uni_m_der,
+        prefix_b,
+        prefix_length
+    ) == True
+    assert verify_prefix_with_cert(
+        ripe_m_pem_path,
+        [],
+        uni_m_der,
+        prefix_b,
+        prefix_length
+    ) == False
+    assert verify_prefix_with_cert(
+        ripe_m_pem_path,
+        [dfn_m_der, uni_m_der, hpi_m_der],
+        router0_m_der,
+        prefix_b,
+        prefix_length
+    ) == True
+    assert verify_prefix_with_cert(
+        ripe_m_pem_path,
+        [dfn_m_der, uni_m_der, hpi_m_der],
+        router0_m_der,
+        prefix_b,
+        prefix_length - 1
+    ) == False
+    assert verify_prefix_with_cert(
+        ripe_m_pem_path,
+        [],
+        dfn_m_der,
+        prefix_bad,
+        prefix_length
+    ) == False
 
 def test_verify_signature():
-    assert verify_signature(router0_o, signed_ra, ra_signature) == True
-    assert verify_signature(router1_o, signed_ra, ra_signature) == False
-    assert verify_signature(router2_o, signed_ra, ra_signature) == False
-    assert verify_signature(router3_o, signed_ra, ra_signature) == False
+    assert verify_signature(router0_o_der, signed_ra, ra_signature) == True
+    assert verify_signature(router1_o_der, signed_ra, ra_signature) == False
+    assert verify_signature(router2_o_der, signed_ra, ra_signature) == False
+    assert verify_signature(router3_o_der, signed_ra, ra_signature) == False
 
 def test_verify_cert():
-    assert verify_cert(ripe_o, None, dfn_o) == True
-    assert verify_cert(ripe_o, dfn_o, uni_o) == True
-    assert verify_cert(ripe_o, None, uni_o) == False
-    assert verify_cert(ripe_o, dfn_uni_hpi_o, router1_o) == True
-    assert verify_cert(ripe_o, dfn_uni_hpi_o, router2_o) == False
-    assert verify_cert(ripe_o, dfn_uni_hpi_o, router3_o) == False
-    assert verify_cert(ripe_m, dfn_uni_hpi_m, router0_m) == True
-
+    assert verify_cert(ripe_o_pem_path, [], dfn_o_der) == True
+    assert verify_cert(ripe_o_pem_path, [dfn_o_der], uni_o_der) == True
+    assert verify_cert(ripe_o_pem_path, [], uni_o_der) == False
+    assert verify_cert(ripe_o_pem_path, [dfn_o_der, uni_o_der, hpi_o_der], router1_o_der) == True
+    assert verify_cert(ripe_o_pem_path, [dfn_o_der, uni_o_der, hpi_o_der], router2_o_der) == False
+    assert verify_cert(ripe_o_pem_path, [dfn_o_der, uni_o_der, hpi_o_der], router3_o_der) == False
+    assert verify_cert(ripe_m_pem_path, [dfn_m_der, uni_m_der, hpi_m_der], router0_m_der) == True
 
 def run_tests():
     test_verify_cert()
