bf915ecc13aa1f2f24895ef13bc51cfdc318108f
Michael Goderbauer <contact@m-goderbauer.de>
2012-02-21 19:42:36 +0100

    modified setup.py

--- client/usermode/security/setup.py
+++ client/usermode/security/setup.py
@@ -1,8 +1,15 @@
+import platform
 from distutils.core import setup, Extension
+
+
+if platform.system() == "Darwin":
+	lib_dirs = ['./lib/MacOS/x64', './lib/MacOS/ia32']
+# TODO: other OSs
+
 setup(name="security", version="1.0", ext_modules=[Extension(
         "security",
         libraries=['crypto'],
-        library_dirs=['./lib/x64', './lib/ia32'],
+        library_dirs=lib_dirs,
         include_dirs=['./include'],
         sources=["security.c"],
         define_macros=[("DEBUG", None)],
