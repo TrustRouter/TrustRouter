dc06df7a485f77f7a05257fd67da8feea55faa88
Thomas Zimmermann <thomaszimmermann1@gmail.com>
2012-03-02 16:22:06 +0100

    moved service.py and winservice.py one level up

--- client/usermode/service.py
+++ client/usermode/service.py
@@ -1,13 +1,13 @@
 from winservice import Service, instart
 
-import windows
+import trustrouter.windows
 
 class TrustRouterService(Service):
     def start(self):
         self.log("TrustRouter Started")
         self.runflag = True
         self.log("TrustRouter Run")
-        windows.run(self.log) 
+        trustrouter.windows.run(self.log) 
     def stop(self):
         self.runflag = False
         self.log("I'm done")
