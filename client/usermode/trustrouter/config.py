ebd391f38289cb5044c7bcbcbb5d6835974a48c7
Michael Goderbauer <contact@m-goderbauer.de>
2012-04-03 15:34:58 +0200

    fixed hop limit for cps

--- client/usermode/trustrouter/config.py
+++ client/usermode/trustrouter/config.py
@@ -64,4 +64,4 @@ class Config(object):
             return value
         elif value is not None:
             log("Invalid config option for NDPROTECTOR_COMPATIBILITY: Must be a bool.")
-        return True
+        return True 
